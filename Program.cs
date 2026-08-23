ddc5be817ee1a32fed7d2a578e6e5582a8ac17ec
Sargeant314 <ctsargeant598@gmail.com>
2026-08-23 23:54:30 +0000

    fixed bug in Program.cs, controller now works properly

--- Program.cs
+++ Program.cs
@@ -22,6 +22,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
